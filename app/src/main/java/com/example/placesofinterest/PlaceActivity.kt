714f71200b866d10ea01bb853d9740e480501e7f
my vu <myxvux@gmail.com>
2019-09-09 11:21:44 +0200

    rename activity and activity xml

--- app/src/main/java/com/example/placesofinterest/PlaceActivity.kt
+++ app/src/main/java/com/example/placesofinterest/PlaceActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
-import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_place.*
 
-class MainActivity : AppCompatActivity() {
+class PlaceActivity : AppCompatActivity() {
 
     private val places = arrayListOf<Place>()
     private val placeAdapter = PlaceAdapter(places)
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_place)
         initViews()
     }
 
